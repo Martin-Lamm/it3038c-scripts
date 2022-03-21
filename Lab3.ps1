2d93b90c8ebcb8d2d8ecf36590132f532795f8bb
Martin-Lamm <98190807+Martin-Lamm@users.noreply.github.com>
2022-03-20 23:24:42 -0400

    Add files via upload
    I was able to run the script but got a one error for some reason.  I believe it is something to do with the Send-MailMessage line.  But other than that I had everything working.

---
+++ Lab3.ps1
@@ -0,0 +1,24 @@
+﻿function getIP{
+
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+    }
+Write-Host(getIP)
+$IP = getIP
+
+function GetVersion{
+    Get-Host | Select-Object Version
+    }
+Write-Host(GetVersion)
+$Version = GetVersion
+
+function GetLocalUser{
+    Get-LocalUser
+    }
+
+function GetDate{
+    Get-Date
+    }
+Write-Host(GetDate)
+$Date = GetDate
+
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "lam.martin31@gmail.com" Subject "IT3038c Windows SysInfo" -Body "This is my lab 3 script!  IP Address: $IP, Powershell Version: $Version, Local User: $env:USERNAME, Date: $Date." -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
